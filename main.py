084617e35231b38728f00698072127dd29ae7d68
Анна <annaandreeva18@yandex.ru>
2023-06-27 22:59:32 +0400

    Ex8

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from json.decoder import JSONDecodeError
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+response = requests.post("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+print(response.status_code)
+
+# try:
+#     parsed_response_text = response.json()
+#     print(parsed_response_text)
+# except JSONDecodeError:
+#     print("не json")
+
