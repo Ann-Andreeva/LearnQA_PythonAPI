26a279639aa287c3fd0011cff5b2d2ab01c9d8c6
Анна <annaandreeva18@yandex.ru>
2023-07-03 23:38:34 +0400

    Ex8 (исправленное)

--- II.Ex8.py
+++ II.Ex8.py
@@ -3,12 +3,13 @@ import time
 
 response1 = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job")
 token = response1.json()["token"]
+seconds = response1.json()["seconds"]
 
 response2 = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={"token": token})
 if response2.json()["status"] == "Job is NOT ready":
     print("Status is correct (Job is NOT ready)")
 
-time.sleep(15)
+time.sleep(seconds)
 
 response3 = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={"token": token})
 if response3.json()["status"] == "Job is ready":
